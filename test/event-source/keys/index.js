02ac37847193167df2de5ac6d10df38afb541ece
Boite Witte <boite@vigour.io>
2016-05-25 11:22:57 +0200

    setup key generation performance tests

--- test/event-source/keys/index.js
+++ test/event-source/keys/index.js
@@ -12,17 +12,37 @@ test('create keys', function createKeys (t) {
 })
 
 test('parse keys', function (t) {
+  t.plan(4)
   var timestamp = (new Date()).getTime()
   t.deepEqual(keys.parse(timestamp + '~1.a'), {
     timestamp: timestamp,
-    event: 1,
+    event: '1',
     path: 'a',
-    stamp: {val: 'a'}
+    stamp: {val: '1'}
   }, 'should return timestamp, event, path and stamp')
-  t.deepEqual(keys.parse(timestamp + '~1.a.b.c'), {
+  t.deepEqual(keys.parse(timestamp + '~2.a.b'), {
     timestamp: timestamp,
-    event: 1,
-    path: 'a.b.c',
-    stamp: {val: 'a'}
+    event: '2',
+    path: 'a.b',
+    stamp: {val: '2'}
   }, 'should return timestamp, event, deepPath and stamp')
+  t.deepEqual(keys.parse(timestamp + '~click-3.a.b.c'), {
+    timestamp: timestamp,
+    event: 'click-3',
+    path: 'a.b.c',
+    stamp: {
+      type: 'click',
+      val: '3'
+    }
+  }, 'should return timestamp, event, deepPath and stamp with type')
+  t.deepEqual(keys.parse(timestamp + '~source|3.a.b.c'), {
+    timestamp: timestamp,
+    event: 'source|3',
+    path: 'a.b.c',
+    stamp: {
+      src: 'source',
+      val: '3'
+    }
+  }, 'should return timestamp, event, deepPath and stamp with source')
+  t.end()
 })
