18e35cba543e0c2252fa91890e95adbab2dd4bc1
Boite Witte <boite@vigour.io>
2016-05-25 10:37:52 +0200

    Added tests for keys

---
+++ test/construct.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var test = require('tape')
+var Observable = require('vigour-observable')
+var levelup = require('levelup')
+var now = new Date()
+var dbName = 'test-' + now.getFullYear() + (now.getMonth() + 1) + now.getDate() + '-' + now.getHours() + now.getMinutes()
+var obs
+
+test('connect with leveldb', function (t) {
+  t.plan(2)
+  obs = new Observable({
+    db: {
+      inject: require('../lib'),
+      name: dbName
+    }
+  })
+  t.equal(obs.db.name.val, dbName, 'The database name should be set and correct')
+  console.log('obs', obs)
+  t.ok(obs.db.integral instanceof levelup, 'db should now contain a levelUp object')
+})
--- test/event-source/keys/index.js
+++ test/event-source/keys/index.js
@@ -3,11 +3,26 @@
 const keys = require('../../../lib/event-source/keys')
 const test = require('tape')
 
-test('create keys', function (t) {
-  t.plan(2)
-  t.equal('', keys.generate ('', '', '')
+test('create keys', function createKeys (t) {
+  t.plan(3)
+  t.equal(/[0-9]+\~1\.a/i.test(keys.generate(1, 'a')), true, 'should return "{timestamp}~1.a"')
+  t.equal(/[0-9]+\~click-2\.a\.b/i.test(keys.generate('click-2', 'a.b')), true, 'should return "{timestamp}~click-2.a.b"')
+  t.equal(/[0-9]+\~mac\|3\.a\.b\.c/i.test(keys.generate('mac|3', 'a.b.c')), true, 'should return "{timestamp}~mac|3.a.b.c"')
+  t.end()
 })
 
 test('parse keys', function (t) {
-
+  var timestamp = (new Date()).getTime()
+  t.deepEqual(keys.parse(timestamp + '~1.a'), {
+    timestamp: timestamp,
+    event: 1,
+    path: 'a',
+    stamp: {val: 'a'}
+  }, 'should return timestamp, event, path and stamp')
+  t.deepEqual(keys.parse(timestamp + '~1.a.b.c'), {
+    timestamp: timestamp,
+    event: 1,
+    path: 'a.b.c',
+    stamp: {val: 'a'}
+  }, 'should return timestamp, event, deepPath and stamp')
 })
