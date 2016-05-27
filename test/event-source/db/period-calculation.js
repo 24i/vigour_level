444e47e71352f8743228a26b7350b96118d632eb
Boite Witte <boite@vigour.io>
2016-05-27 10:42:40 +0200

    Calculate periods (not time ready yet)

--- test/event-source/db/period-calculation.js
+++ test/event-source/db/period-calculation.js
@@ -46,7 +46,7 @@ test('should calculate periods with Week designator', function (t) {
   t.end()
 })
 
-test('should calculate periods with Month designator', function (t) {
+test('should calculate periods with Day designator', function (t) {
   t.deepEqual(periodCalculation('P1D', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 4, 25, 0, 0, 0, 0))})
   t.deepEqual(periodCalculation('P1D', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 4, 26, 0, 0, 0, 0))})
   t.deepEqual(periodCalculation('P1D', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 4, 27, 0, 0, 0, 0))})
@@ -54,6 +54,54 @@ test('should calculate periods with Month designator', function (t) {
   t.deepEqual(periodCalculation('P5D', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 4, 20, 0, 0, 0, 0))})
   t.deepEqual(periodCalculation('P5D', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 4, 25, 0, 0, 0, 0))})
   t.deepEqual(periodCalculation('P5D', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 4, 30, 0, 0, 0, 0))})
+
+  t.end()
+})
+
+test('should calculate periods with Year and Month designator', function (t) {
+  t.deepEqual(periodCalculation('P1Y1M', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2015, 0, 1, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1M', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 1, 1, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1M', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2017, 2, 1, 0, 0, 0, 0))})
+
+  t.deepEqual(periodCalculation('P2Y3M', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2013, 9, 1, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P2Y3M', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 0, 1, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P2Y3M', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2018, 3, 1, 0, 0, 0, 0))})
+
+  t.end()
+})
+
+test('should calculate periods with Year and Week designator', function (t) {
+  t.deepEqual(periodCalculation('P1Y1W', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2014, 11, 29, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1W', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 0, 11, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1W', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2017, 0, 16, 0, 0, 0, 0))})
+
+  t.deepEqual(periodCalculation('P3Y4W', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2012, 3, 16, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P3Y4W', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2015, 4, 11, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P3Y4W', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2018, 5, 11, 0, 0, 0, 0))})
+
+  t.end()
+})
+
+test('should calculate periods with Year and Day designator', function (t) {
+  t.deepEqual(periodCalculation('P1Y1D', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2014, 6, 1, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1D', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2015, 6, 2, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1D', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 6, 3, 0, 0, 0, 0))})
+
+  t.deepEqual(periodCalculation('P4Y5D', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2010, 10, 10, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P4Y5D', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2014, 10, 15, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P4Y5D', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2018, 10, 20, 0, 0, 0, 0))})
+
+  t.end()
+})
+
+test('should calculate periods with Year, Month and Day designator', function (t) {
+  t.deepEqual(periodCalculation('P1Y1M1D', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2014, 7, 29, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1M1D', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2015, 8, 30, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y1M1D', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 10, 1, 0, 0, 0, 0))})
+
+  t.deepEqual(periodCalculation('P1Y2M3D', -1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2014, 11, 2, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y2M3D', false, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2016, 1, 5, 0, 0, 0, 0))})
+  t.deepEqual(periodCalculation('P1Y2M3D', 1, new Date(2016, 4, 26, 10, 45, 20, 100)), {start: new Date(Date.UTC(2017, 3, 8, 0, 0, 0, 0))})
 
   t.end()
 })
