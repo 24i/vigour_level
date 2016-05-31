fcb9ba2243ee8df05c17d873e568d66e28138942
Boite Witte <boite@vigour.io>
2016-05-31 15:48:20 +0200

    Observable set & deepSet working + cleanup

--- test/observable/set.js
+++ test/observable/set.js
@@ -9,26 +9,30 @@ var executed = {
   base: {
     event: false,
     data: false
+  },
+  deep: {
+    event: false,
+    data: false
   }
 }
 
-module.exports = function observableSetTest (obs, dbName) {
+module.exports = function observableSetTest (obs, dbName, allDoneCB) {
   test('observable set on base via queue', function (t) {
     var setData = {a: {b: 'a'}}
     t.plan(4)
     var stampQueue = vstamp.create('testQueue', 'obs')
     obs.db.integral._getDb('current').on('put', function (key, value) {
       if (executed.queue.event === false) {
-        t.equal(key, stampQueue + '.$ROOT')
-        t.deepEqual(value, setData, 'value should be same as set data')
+        t.equal(key, stampQueue + '.$ROOT.a.b')
+        t.equal(value, setData.a.b, 'value should be same as set data')
         executed.queue.event = true
         allIsDone(executed.queue, testSetBase)
       }
     })
     obs.db.integral._getDb('data').on('put', function (key, value) {
       if (executed.queue.data === false) {
-        t.equal(key, '$ROOT')
-        t.deepEqual(value, setData, 'value should be same as set data')
+        t.equal(key, '$ROOT.a.b')
+        t.equal(value, setData.a.b, 'value should be same as set data')
         executed.queue.data = true
         allIsDone(executed.queue, testSetBase)
       }
@@ -42,16 +46,16 @@ module.exports = function observableSetTest (obs, dbName) {
       var stamp = vstamp.create('testBase', 'obs')
       obs.db.integral._getDb('current').on('put', function (key, value) {
         if (executed.base.event === false) {
-          t.equal(key, stamp + '.$ROOT', 'event key should be stamp + base + key')
-          t.deepEqual(value, setBaseData, 'value should be same as set data')
+          t.equal(key, stamp + '.$ROOT.c', 'event key should be stamp + base + key')
+          t.equal(value, setBaseData.c, 'value should be same as set data')
           executed.base.event = true
           allIsDone(executed.base, testDeepSet)
         }
       })
       obs.db.integral._getDb('data').on('put', function (key, value) {
         if (executed.base.data === false) {
-          t.equal(key, '$ROOT')
-          t.deepEqual(value, setBaseData, 'value should be same as set data')
+          t.equal(key, '$ROOT.c')
+          t.deepEqual(value, setBaseData.c, 'value should be same as set data')
           executed.base.data = true
           allIsDone(executed.base, testDeepSet)
         }
@@ -65,19 +69,19 @@ module.exports = function observableSetTest (obs, dbName) {
       t.plan(4)
       var stamp = vstamp.create('testDeep', 'obs')
       obs.db.integral._getDb('current').on('put', function (key, value) {
-        if (executed.base.event === false) {
-          t.equal(key, stamp + '.$ROOT.a', 'event key should be stamp + base + key')
-          t.deepEqual(value, setDeepData, 'value should be same as set data')
+        if (executed.deep.event === false) {
+          t.equal(key, stamp + '.$ROOT.a.d', 'event key should be stamp + base + key')
+          t.equal(value, setDeepData.d, 'value should be same as set data')
           executed.base.event = true
-        // allIsDone(executedBase, testSetBase)
+          allIsDone(executed.deep, allDoneCB)
         }
       })
       obs.db.integral._getDb('data').on('put', function (key, value) {
-        if (executed.base.data === false) {
-          t.equal(key, '$ROOT.a')
-          t.deepEqual(value, setDeepData, 'value should be same as set data')
+        if (executed.deep.data === false) {
+          t.equal(key, '$ROOT.a.d')
+          t.deepEqual(value, setDeepData.d, 'value should be same as set data')
           executed.base.data = true
-        // allIsDone(executedBase, testSetBase)
+          allIsDone(executed.deep, allDoneCB)
         }
       })
       obs.a.set(setDeepData, stamp)
